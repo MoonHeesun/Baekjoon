88e94dec92ea31304512adc7a0e30e5e4d5dac86
MoonHuiseon <127913600+MoonHuiseon@users.noreply.github.com>
2023-07-08 23:02:02 +0900

    Update 오븐 시계.py

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,3 +1,5 @@
+## 방식 1
+
 h, m = map(int, input().split())
 t = int(input())
 if m+t < 60:
@@ -7,3 +9,13 @@ else:
         print(h+((m+t)//60), ((m+t)%60))
     else:
         print(((h+((m+t)//60)))-24, ((m+t)%60))
+
+## 방식 2
+
+# h, m = map(int, input().split())
+# t = int(input())
+        
+# final_m = h * 60 + m + t
+# final_h = (final_m // 60) % 24
+# final_m = final_m % 60
+# print(final_h, final_m)
