228d7e802de896288562afb7cc348e9649b1b83f
MoonHuiseon <127913600+MoonHuiseon@users.noreply.github.com>
2023-07-15 10:29:18 +0900

    Update 색종이.py

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -6,8 +6,8 @@ for i in range(N):
     X, Y = map(int, input().split())
     for col in range(X, X+10):
         for row in range(Y, Y+10):
-            if canvas[row-1][col-1] == 0:
-                canvas[row-1][col-1] += 1
+            if canvas[row][col] == 0:
+                canvas[row][col] += 1
                 area += 1
             else:
                 canvas[row][col] += 0
