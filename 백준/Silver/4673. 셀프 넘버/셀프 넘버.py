1c383f47ea8b9e7d24343b6022c79d0f6632a9d2
MoonHuiseon <127913600+MoonHuiseon@users.noreply.github.com>
2023-07-25 02:04:59 +0900

    [Silver V] Title: 셀프 넘버, Time: 208 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,15 +1,17 @@
-def d(n):
-    ans = n
-    num = list(str(n))
-    for i in range(len(num)):
-        ans += int(num[i])
-    return(ans)
+array = [i for i in range(1,10001)]
 
-array = list(range(1,10001))
-
-for i in range(1, 10001):
-    if d(i) in array:
-        array.remove(d(i))
+for start in range(1, 10001):
+    share = start
+    d = 0
+    while share != 0:
+        rest = share%10
+        share = share//10
+        d += rest
+    next = start+d+share
+    if next in array:
+        array.remove(next)
+    else:
+        continue
 
 for i in array:
     print(i)
