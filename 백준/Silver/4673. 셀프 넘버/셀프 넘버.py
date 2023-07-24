aff7ef8471c345d6ddedea11db23c4c9389ec3af
MoonHuiseon <127913600+MoonHuiseon@users.noreply.github.com>
2023-07-25 02:07:05 +0900

    Update 셀프 넘버.py

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,17 +1,36 @@
-array = [i for i in range(1,10001)]
+# 방법 1
+def d(n):
+    ans = n
+    num = list(str(n))
+    for i in range(len(num)):
+        ans += int(num[i])
+    return(ans)
 
-for start in range(1, 10001):
-    share = start
-    d = 0
-    while share != 0:
-        rest = share%10
-        share = share//10
-        d += rest
-    next = start+d+share
-    if next in array:
-        array.remove(next)
-    else:
-        continue
+array = list(range(1,10001))
+
+for i in range(1, 10001):
+    if d(i) in array:
+        array.remove(d(i))
 
 for i in array:
     print(i)
+
+
+# 방법 2
+# array = [i for i in range(1,10001)]
+
+# for start in range(1, 10001):
+#     share = start
+#     d = 0
+#     while share != 0:
+#         rest = share%10
+#         share = share//10
+#         d += rest
+#     next = start+d+share
+#     if next in array:
+#         array.remove(next)
+#     else:
+#         continue
+
+# for i in array:
+#     print(i)
