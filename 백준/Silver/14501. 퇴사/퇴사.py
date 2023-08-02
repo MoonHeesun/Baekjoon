471c2a7c7e2ca84c8547c316e0abb34ebbcf6bd6
MoonHuiseon <127913600+MoonHuiseon@users.noreply.github.com>
2023-08-03 00:42:43 +0900

    [Silver III] Title: 퇴사, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,23 @@
+n = int(input())
+t_list = []
+p_list = []
+for day in range(n):
+    t, p = map(int, input().split())
+    t_list.append(t)
+    p_list.append(p)
+
+def fn(curr, total_pay):
+    global answer
+    if curr == n:
+        answer = max(total_pay, answer)
+        return
+    if (curr+t_list[curr]) <= n:
+        fn(curr+t_list[curr], total_pay+p_list[curr])
+    # fn(curr+1, total_pay)
+    fn(curr+1, total_pay)
+
+answer = 0
+res = []
+for i in range(n):
+    fn(i, 0)
+print(answer)
